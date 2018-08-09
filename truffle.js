0fe729b5cae656b59e9efc8b01a1b4a83db3cf5e
thanhnguyennguyen <nguyenbk92@gmail.com>
2018-08-09 12:59:17 +0700

    Truffle test

--- truffle.js
+++ truffle.js
@@ -1,6 +1,6 @@
 // npm install -g truffle-hdwallet-provider
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic;
+// var HDWalletProvider = require("truffle-hdwallet-provider");
+// var mnemonic;
 module.exports = {
   networks: {
     ganache: {
@@ -8,11 +8,11 @@ module.exports = {
       port: 8545,
       network_id: "5777"
     },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/<INFURA_Access_Token>")
-      },
-      network_id: 3
-    }
+    // ropsten: {
+    //   provider: function() {
+    //     return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/<INFURA_Access_Token>")
+    //   },
+    //   network_id: 3
+    // }
   }
 };
