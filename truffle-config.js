276d0fa129d05c284fb7d6014ca1ea2a2d701960
thanhnguyennguyen <nguyenbk92@gmail.com>
2018-08-09 15:41:42 +0700

    solidity test coverage

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,25 @@
+// npm install -g truffle-hdwallet-provider
+// var HDWalletProvider = require("truffle-hdwallet-provider");
+// var mnemonic;
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    ganache: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "5777"
+    },
+    // ropsten: {
+    //   provider: function() {
+    //     return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/<INFURA_Access_Token>")
+    //   },
+    //   network_id: 3
+    // }
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01      // <-- Use this low gas price
+    }
+  }
 };
